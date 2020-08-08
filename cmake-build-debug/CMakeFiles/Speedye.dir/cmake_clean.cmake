9c5a9d5f72c02f37ce8ac08ce2430c7fabc54286
cxzlw <wsc_zds@qq.com>
2020-08-08 18:18:30 +0800

    整理下代码

---
+++ cmake-build-debug/CMakeFiles/Speedye.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Speedye.dir/src/DESIGNER.cpp.obj"
+  "Speedye.exe"
+  "Speedye.exe.manifest"
+  "Speedye.pdb"
+  "libSpeedye.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Speedye.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
